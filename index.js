5b8d66089e1530ae9fb4390a71d60c963cef902a
Dilan Silva <sdilansilva@gmail.com>
2021-03-06 22:14:48 +0530

    Login route is done

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
 const express = require('express');//import some express apps
-const mysql = require('mysql');//import some mysql apps
+const mongodb = require('mongodb');//mongodb package
 
 const app = express();
 app.use(express.json());
 
-var mysqlConnection = mysql.createConnection({
-    host : "localhost",
-    user : "yourusername",
-    password : "password",
-    database : "EmployeeDB"
-});
+const MongoClient = mongodb.MongoClient;//initialize the connection
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'Projects';//database name
 
-mysqlConnection.connect((err) => {
-    if(!err) 
-        console.log('Connected Successful!');
-    else 
-        console.log('Connection failed!');
-});
 
 app.post('/login', (req, res) => {//route for login
-    console.log('Login!');
+    console.log(req.body);
+
+    MongoClient.connect(connectionURL, {useNewUrlParser : true,useUnifiedTopology: true}, (error, client) => {//connect to database
+        if(error){
+            return console.error('error');
+        }else{
+            const db = client.db(databaseName);
+            db.collection('user').findOne({
+                user_mail : req.body.user_mail
+            },(error,result) => {
+                if(error){
+                    console.log(error);
+                }
+                if(result != null){
+                    db.collection('user').findOne({
+                        user_password : req.body.user_password   
+                    },(error,result) => {
+                        if(error){
+                            console.log(error);
+                        }if(result != null){
+                            res.send('Logging');
+                        }else{
+                            res.send('Wrong password!');
+                        }
+                    })
+                }else{
+                    return res.send('Unmatched email!');
+                }
+            })
+        }
+    });
+    
+
+    
 });
 
 app.post('/customer', (req,res) => {//route for enter customer details
